5e8f2f7845f0a4974477526a6c2513e613098771
Lorenzo V <lorenzo.vella@live.com>
2020-07-01 17:18:36 -0300

    deploy attempt 2

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -55,7 +55,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'Manada.urls'
+ROOT_URLCONF = 'Portfolio.urls'
 
 TEMPLATES = [
     {
@@ -73,7 +73,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'Manada.wsgi.application'
+WSGI_APPLICATION = 'Portfolio.wsgi.application'
 
 
 # Database
